2ad77678d9afa2e7932f5b9388aad3772d7353ee
kokolegorille <koko.le.gorille@gmail.com>
2021-08-20 22:42:46 +0200

    Initial commit

---
+++ lib/file_store.ex
@@ -0,0 +1,18 @@
+defmodule FileStore do
+  @moduledoc """
+  Documentation for `FileStore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FileStore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
